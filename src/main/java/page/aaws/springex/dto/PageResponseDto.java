85c54b0ccad007c8f4edad5163cd5190c4f0ee0c
yongmoon jo <yongmoon@hanmail.net>
2023-09-21 14:38:18 +0900

    [a01] 주석 추가

--- src/main/java/page/aaws/springex/dto/PageResponseDto.java
+++ src/main/java/page/aaws/springex/dto/PageResponseDto.java
@@ -9,16 +9,44 @@ import java.util.List;
 @Getter
 @ToString
 public class PageResponseDto<E> {
+    /**
+     * 현재 페이지 번호
+     */
     private int page;
+
+    /**
+     * 페이지 당 아이템 개수
+     */
     private int size;
+
+    /**
+     * 총 아이템 개수
+     */
     private int itemCount;
 
+    /**
+     * 내비게이션 시작 페이지 번호
+     */
     private int start;
+
+    /**
+     * 내비게이션 종료 페이지 번호
+     */
     private int end;
 
+    /**
+     * 내비게이션 시작 페이지의 이전 페이지 존재 여부
+     */
     private boolean prev;
+
+    /**
+     * 내비게이션 종료 페이지의 이후 페이지 존재 여부
+     */
     private boolean next;
 
+    /**
+     * 아이템 목록
+     */
     private List<E> dtoList;
 
     /**
