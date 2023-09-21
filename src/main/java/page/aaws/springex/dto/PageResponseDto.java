07426f8ddb223b4bfd9661fadd7317e7b0b7e9a0
yongmoon jo <yongmoon@hanmail.net>
2023-09-21 15:07:11 +0900

    [a01] PageResponseDto.prev, next 멤버명 수정

--- src/main/java/page/aaws/springex/dto/PageResponseDto.java
+++ src/main/java/page/aaws/springex/dto/PageResponseDto.java
@@ -6,62 +6,78 @@ import lombok.ToString;
 
 import java.util.List;
 
-@Getter
 @ToString
 public class PageResponseDto<E> {
     /**
      * 현재 페이지 번호
      */
+    @Getter
     private int page;
 
     /**
      * 페이지 당 아이템 개수
      */
+    @Getter
     private int size;
 
     /**
      * 총 페이지 개수
      */
+    @Getter
     private int count;
 
     /**
      * 총 아이템 개수
      */
+    @Getter
     private int itemCount;
 
     /**
      * 내비게이션 시작 페이지 번호
      */
+    @Getter
     private int start;
 
     /**
      * 내비게이션 종료 페이지 번호
      */
+    @Getter
     private int end;
 
     /**
      * 내비게이션 시작 페이지의 이전 페이지 번호
      */
+    @Getter
     private int lastPageOfPreviousBand;
 
     /**
      * 내비게이션 종료 페이지의 다음 페이지 번호
      */
+    @Getter
     private int startPageOfNextBand;
 
     /**
      * 내비게이션 시작 페이지의 이전 페이지 존재 여부
      */
-    private boolean prev;
+    private boolean previousBandExist;
 
     /**
      * 내비게이션 종료 페이지의 이후 페이지 존재 여부
      */
-    private boolean next;
+    private boolean nextBandExist;
+
+    public boolean doesPreviousBandExist() {
+        return this.previousBandExist;
+    }
+
+    public boolean doesNextBandExist() {
+        return this.nextBandExist;
+    }
 
     /**
      * 아이템 목록
      */
+    @Getter
     private List<E> dtoList;
 
     @Builder(builderMethodName = "withAll")
@@ -78,15 +94,12 @@ public class PageResponseDto<E> {
         this.start = this.end - 9;
 
         if (this.start > 1) {
+            this.previousBandExist = true;
             this.lastPageOfPreviousBand = this.start - 1;
         }
         if (this.end < this.count) {
+            this.nextBandExist = true;
             this.startPageOfNextBand = this.end + 1;
         }
-
-        int last = (int)(Math.ceil((itemCount /(double)size)));
-        this.end = Math.min(end, last);
-        this.prev = this.start > 1;
-        this.next = itemCount > this.end * this.size;
     }
 }
